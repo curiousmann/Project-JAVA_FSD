55984a300a4d72614830685a59840e0e8471d3b1
Manu Joy <manujoyolickal@gmail.com>
2022-03-10 19:21:55 +0530

    home page

--- Angular_project/ang_v1.5_nek/src/app/app-routing.module.ts
+++ Angular_project/ang_v1.5_nek/src/app/app-routing.module.ts
@@ -3,14 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { HomeComponent } from './home/home.component';
-import { ProductComponent } from './product/product.component';
+
 
 const routes: Routes = [
   //{path:'',component:HomeComponent},
-  {path:'home',component:HomeComponent},
   {path:'about',component:AboutusComponent},
   {path:'contact',component:ContactusComponent},
-
+  {path:'home',component:HomeComponent},
 ];
 
 @NgModule({
